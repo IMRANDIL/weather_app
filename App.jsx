25b1f2c6a1e63c6ae07aacad72bd609ba1d3f9a1
IMRANDIL <aliimranadil2@gmail.com>
2023-01-28 12:53:45 +0530

    some more change

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {Text} from 'react-native';
+import Details from './src/Details';
 import Home from './src/Home';
 
 const Stack = createNativeStackNavigator();
@@ -11,6 +12,7 @@ const App = () => {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{headerShown: false}}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Details" component={Details} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'fe29772038bff3e5f98746d930fa0084';
