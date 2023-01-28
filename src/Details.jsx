9c74427b994487a9888ef3e0c5bdc4ab450b224e
IMRANDIL <aliimranadil2@gmail.com>
2023-01-28 13:56:17 +0530

    some more change

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
-import {Text} from 'react-native';
-
+import {
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {deviceHeight, deviceWidth} from './Diamension';
+import Icon from 'react-native-vector-icons/Ionicons';
 const Details = props => {
   const {name} = props.route.params;
   console.log('name', name);
-  return <Text style={{fontSize: 22}}>{name}</Text>;
+  return (
+    <View>
+      <ImageBackground
+        source={require('../assets/images/image2.jpg')}
+        style={{height: deviceHeight, width: deviceWidth}}
+        imageStyle={{opacity: 0.6, backgroundColor: 'black'}}
+      />
+      <View
+        style={{
+          position: 'absolute',
+          paddingHorizontal: 10,
+          paddingVertical: 20,
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: deviceWidth - 20,
+          }}>
+          <TouchableOpacity>
+            <Icon name="menu" size={46} color="white" />
+          </TouchableOpacity>
+          <Image
+            source={require('../assets/images/user.jpg')}
+            style={{height: 46, width: 46, borderRadius: 50}}
+          />
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default Details;
