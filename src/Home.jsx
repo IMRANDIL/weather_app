4482099d3d6f3e70cda9238007089aa4bd957742
IMRANDIL <aliimranadil2@gmail.com>
2023-01-28 08:52:03 +0530

    some more change

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {ImageBackground, Text, View} from 'react-native';
+import {deviceHeight, deviceWidth} from './Diamension';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <ImageBackground
+        source={require('../assets/images/image2.jpg')}
+        style={{height: deviceHeight, width: deviceWidth}}
+      />
     </View>
   );
 };
